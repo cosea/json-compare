0a2facc7c64c551e14fe7ce9b1661b3606bc6a97
meredrica <stuff@meredrica.org>
2014-01-16 00:42:40 +0100

    initial snafu

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-couchdb-json-compare
-====================
+json-compare
+=============
+A simple comparator for json files that are reachable via an URI.
 
-a simple comparator for json files that reside in couchdb
+The goal of this tool was to test backends that use couchdb and validate that they generate the correct json files after substantial changes were made to them.
+
+		Usage:
+			jsoncompare [options]
+			where [options] are:
+					 --original, -o <s>:   The URI to the original JSON, can be a file or an url
+							--check, -c <s>:   The URI to the JSON under test, can be a file or an url
+			--auth-original, -a <s>:   The user:pass combination to use in http basic auth for the original (default: )
+				 --auth-check, -u <s>:   The user:pass combination to use in http basic auth for the json under test (default: )
+								--version, -v:   Print version and exit
+									 --help, -h:   Show this message
---
+++ compare.rb
@@ -0,0 +1,54 @@
+require 'hashdiff'
+require 'open-uri'
+require 'json'
+require 'trollop'
+
+
+opts = Trollop::options do
+	version "0.0.1 (c) by COSEA. Released under modified BSD. See LICENSE.txt for details."
+
+	banner <<-EOS
+	Simple JSON comparator that can read files or urls and compares them
+
+	Usage:
+	ruby compare.rb [options]
+	where [options] are:
+	EOS
+
+	opt :original, "The URI to the original JSON, can be a file or an url", type: :string
+	opt :check, "The URI to the JSON under test, can be a file or an url", type: :string
+	opt 'auth-original', "The user:pass combination to use in http basic auth for the original", type: :string, default:'' 
+	opt 'auth-check', "The user:pass combination to use in http basic auth for the json under test", type: :string, default:'' 
+end
+
+Trollop::die :original, "must be given" unless opts[:original]
+Trollop::die :check, "must be given" unless opts[:check]
+
+original_file = 'original.json'
+check_file = 'check.json'
+
+# download both files
+#File.open(original_file,'wb') do |f|
+#	f.write open(opts[:original],http_basic_authentication: opts['auth-original'].split(':')).read
+#end
+
+#File.open(check_file,'wb') do |f|
+#	f.write open(opts[:check],http_basic_authentication: opts['auth-check'].split(':') ).read
+#end
+
+original_data = JSON.parse open(opts[:original],http_basic_authentication: opts['auth-original'].split(':')).read
+check_data = JSON.parse open(opts[:check],http_basic_authentication: opts['auth-check'].split(':')).read
+
+# parse them into hashes
+#original_data = JSON.parse IO.read(original_file)
+#check_data = JSON.parse IO.read(check_file)
+
+diff = HashDiff.diff(original_data,check_data)
+
+if diff.size == 0
+	puts 'no difference'
+	exit 0
+else
+	puts diff
+	exit 1
+end

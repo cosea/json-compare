d955c37f8e057a34e93790f8e7954d5641e0dd92
meredrica <stuff@meredrica.org>
2014-01-23 00:37:04 +0100

    removes dependency on hashdiff gem
    implements hash compare manually because we need to manipulate arrays
    
    resolves #1
    resolves #4

--- compare.rb
+++ compare.rb
@@ -1,7 +1,7 @@
-require 'hashdiff'
 require 'open-uri'
 require 'json'
 require 'trollop'
+require './hashdiff'
 
 opts = Trollop::options do
 	version "0.0.1 (c) by COSEA. Released under modified BSD. See LICENSE.txt for details."
@@ -18,24 +18,20 @@ opts = Trollop::options do
 	opt :check, "The URI to the JSON under test, can be a file or an url", type: :string
 	opt 'auth-original', "The user:pass combination to use in http basic auth for the original", type: :string, default:'' 
 	opt 'auth-check', "The user:pass combination to use in http basic auth for the json under test", type: :string, default:'' 
+	opt 'ignore', "List of keys to ignore, comma separated. Keys specified here will always be skipped", type: :string, default:'' 
+	opt 'ignore-array-ordering', "If true, will ignore the ordering of arrays", type: :boolean, default: false
 end
 
 Trollop::die :original, "must be given" unless opts[:original]
 Trollop::die :check, "must be given" unless opts[:check]
 
-
-
 # parse the data into hashes
 original_data = JSON.parse open(opts[:original],http_basic_authentication: opts['auth-original'].split(':')).read
 check_data = JSON.parse open(opts[:check],http_basic_authentication: opts['auth-check'].split(':')).read
 
+options={}
+options[:ignore_array_ordering] = opts['ignore-array-ordering']
+options[:ignored_keys] = opts['ignore'].split(',')
 
-diff = HashDiff.diff(original_data,check_data)
-
-if diff.size == 0
-	puts 'no difference'
-	exit 0
-else
-	puts diff
-	exit 1
-end
+exit 0 if HashDiff.same? original_data,check_data,options
+exit 1
---
+++ hashdiff.rb
@@ -0,0 +1,55 @@
+module HashDiff
+
+
+	# options
+	#
+	# * :ignore_array_ordering
+	# if true, array sorting does not matter
+	#
+	# * :ignored_keys
+	# an array of keys that can be ignored
+	# 
+	def self.same?(left, right, options={})
+		puts "same?:#{left}, #{right}, #{options}"
+		return true if left == right # cheap early exit
+		return false unless left.class == right.class
+		case left
+		when Hash
+			return same_hash? left,right,options
+		when Array
+			return same_array? left,right,options
+		else
+			return left == right
+		end
+	end
+
+	def self.same_hash?(left,right,options = {})
+		ignored_keys = options[:ignored_keys] || []
+		ignored_keys.map!{|t| t.to_sym}
+		(left.keys + right.keys).uniq.each do |key|
+			next if ignored_keys.member? key.to_sym
+			# check if keys exist
+			if left.key?(key) && right.key?(key)
+				return false unless same? left[key],right[key],options
+			else
+				# we miss a non ignorable key. return false
+				puts "key #{key} missing in:#{left.key?(key) ? right : left }"
+				return false
+			end
+		end
+	end
+
+	def self.same_array?(left,right,options) 
+		return false if left.size != right.size # cheap check
+		ignore_array_ordering = options[:ignore_array_ordering] || false
+		if ignore_array_ordering
+			left = left.sort_by{|t| t.to_s}
+			right = right.sort_by{|t| t.to_s}
+		end
+		return true if left == right # early exit
+		left.size.times do |t|
+			return false unless same? left[t],right[t],options
+		end
+	end
+
+end
